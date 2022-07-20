be58eff76daa9b1090a1e3dd2accc2dcd30f49a0
Artem Chiruhin <artemchiruhin@bk.ru>
2022-07-20 14:05:45 +0500

    created products methods for cart

--- app/Http/Controllers/User/ProductController.php
+++ app/Http/Controllers/User/ProductController.php
@@ -12,4 +12,20 @@ class ProductController extends Controller
     {
         return view('user.products.show', compact('product'));
     }
+
+    public function addProductToCart(Product $product)
+    {
+        session()->push('cart', $product->id);
+        return to_route('user.cart.index');
+    }
+
+    public function removeProductFromCart(Product $product)
+    {
+        $products = session()->pull('cart', []);
+        if(($key = array_search($product->id, $products)) !== false) {
+            unset($products[$key]);
+        }
+        session()->put('cart', $products);
+        return to_route('user.cart.index');
+    }
 }
