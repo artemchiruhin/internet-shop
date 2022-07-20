b1ce4d97eba72ad4346706c64c1561e1ce10f487
Artem Chiruhin <artemchiruhin@bk.ru>
2022-07-20 14:07:16 +0500

    created cart layout

--- resources/views/user/cart/index.blade.php
+++ resources/views/user/cart/index.blade.php
@@ -4,24 +4,34 @@
 
 @section('content')
     <div class="container">
-        @forelse($products as $product)
-        <div class="row mt-2">
-            <div class="col-12 col-md-4">
-                <img src="{{ get_image_path($product->image) }}" alt="{{ $product->name }}" style="width: 100%;">
+        <h1 class="my-5">Корзина</h1>
+        @if(count($products) > 0)
+            @foreach($products as $product)
+            <div class="row mt-5 border-bottom border-primary pb-2">
+                <div class="col-12 col-md-4">
+                    <img src="{{ get_image_path($product->image) }}" alt="{{ $product->name }}" style="width: 100%;">
+                </div>
+                <div class="col-12 col-md-8 d-flex flex-column">
+                    <h4 class="text-primary">{{ $product->name }}</h4>
+                    <p>{{ $product->description }}</p>
+                    <p>Цена: <span class="text-primary">{{ number_format($product->price, 2, ',', ' ') }} р.</span></p>
+                    <p>Категория: <span class="text-primary">{{ $product->category->name }}</span></p>
+                    <form action="{{ route('user.products.removeFromCart', $product) }}" method="POST" class="mt-auto">
+                        @csrf
+                        <button class="btn btn-danger">Удалить</button>
+                    </form>
+                </div>
             </div>
-            <div class="col-12 col-md-8">
-                <h4 class="text-primary">{{ $product->name }}</h4>
-                <p>{{ $product->description }}</p>
-                <p>Цена: <span class="text-primary">{{ number_format($product->price, 2, ',', ' ') }} р.</span></p>
-                <p>Категория: <span class="text-primary">{{ $product->category->name }}</span></p>
-                <form action="" method="POST">
+            @endforeach
+            <div class="py-5 mt-5">
+                <h4>Стоимость: <span class="text-primary">{{ $cost }} р.</span></h4>
+                <form action="{{ route('user.cart.makeOrder') }}" method="POST">
                     @csrf
-                    <button class="btn btn-danger">Удалить</button>
+                    <button class="btn btn-primary">Оформить заказ</button>
                 </form>
             </div>
-        </div>
-        @empty
+        @else
             <h4 class="text-center text-primary mt-5">Корзина пуста.</h4>
-        @endforelse
+        @endif
     </div>
 @endsection
