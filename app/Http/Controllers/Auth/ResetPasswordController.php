79faf05adbf94f7498ec30b97c8ca453509f8420
Artem Chiruhin <artemchiruhin@bk.ru>
2022-07-21 14:08:54 +0500

    changed returning message

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -50,6 +50,6 @@ class ResetPasswordController extends Controller
 
         return $status === Password::PASSWORD_RESET
             ? redirect()->route('auth.login')->with('status', __($status))
-            : back()->withErrors(['email' => ['Ошибка в email']]);
+            : back()->withErrors(['email' => [__($status)]]);
     }
 }
