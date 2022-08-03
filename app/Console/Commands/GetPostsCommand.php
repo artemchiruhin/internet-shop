0323e814f0931300f6d0aa80bd37d318138c70c1
Artem Chiruhin <artemchiruhin@bk.ru>
2022-08-03 17:10:39 +0500

    feature/artisan-command | created artisan command to get posts from api

---
+++ app/Console/Commands/GetPostsCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Post;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class GetPostsCommand extends Command
+{
+    protected $signature = 'posts:get';
+
+    protected $description = 'Получить посты с jsonplaceholder';
+
+    public function handle()
+    {
+        $response = Http::get('https://jsonplaceholder.typicode.com/posts');
+        $posts = $response->json();
+        $this->withProgressBar($posts, function($post) {
+            Post::create(['title' => $post['title'], 'description' => $post['body']]);
+        });
+        $this->newLine();
+        $this->info('Посты были добавлены');
+        return 0;
+    }
+}
