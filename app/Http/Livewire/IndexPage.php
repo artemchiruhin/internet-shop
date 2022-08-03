bd6802d8a729f5f513546e2509bb302d6d6232c3
Artem Chiruhin <artemchiruhin@bk.ru>
2022-08-03 15:44:07 +0500

    feature/comparsion | changed comparsion functionality

--- app/Http/Livewire/IndexPage.php
+++ app/Http/Livewire/IndexPage.php
@@ -9,36 +9,100 @@ class IndexPage extends Component
 {
     public $products;
     public $productsForComparsion;
-    public $productDeletedFromComparsion; // понимаю, костыль
+    public $isModalShown;
+    public $firstProductForComparsion;
+    public $secondProductForComparsion;
 
     public function mount()
     {
         $this->productsForComparsion = [];
-        $this->productDeletedFromComparsion = false;
+        $this->showModal(false);
+        $this->firstProductForComparsion = null;
+        $this->secondProductForComparsion = null;
     }
 
     public function render()
     {
-        return view('livewire.index-page');
+        $firstIndex = $this->getIndexOfProduct($this->firstProductForComparsion ? $this->firstProductForComparsion['id'] : 0);
+        $secondIndex = $this->getIndexOfProduct($this->secondProductForComparsion ? $this->secondProductForComparsion['id'] : 0);
+        return view('livewire.index-page', compact('firstIndex', 'secondIndex'));
     }
 
     public function addForComparison($id)
     {
-        $this->productDeletedFromComparsion = false;
-        if(count($this->productsForComparsion) === 2) {
+        $this->showModal(false);
+        if(in_array($id, $this->productsForComparsion)) {
             return;
         }
-        foreach ($this->productsForComparsion as $product) {
-            if($product['id'] === $id) {
-                return;
-            }
-        }
-        $this->productsForComparsion[] = Product::find($id);
+        $this->productsForComparsion[] = $id;
+        $this->setProductsForComparsion();
     }
 
-    public function deleteFromComparison($index)
+    public function deleteFromComparison($id)
     {
+        $index = array_search($id, $this->productsForComparsion);
         unset($this->productsForComparsion[$index]);
-        $this->productDeletedFromComparsion = true;
+        $this->productsForComparsion = array_values($this->productsForComparsion);
+        $this->setProductsForComparsion();
+        $this->showModal(true);
+    }
+
+    protected function getIndexOfProduct($id)
+    {
+        return array_search($id, $this->productsForComparsion);
+    }
+
+    protected function setProductsForComparsion()
+    {
+        $this->firstProductForComparsion = $this->getProduct(0);
+        $this->secondProductForComparsion = $this->getProduct(1);
+    }
+
+    protected function getProduct($index)
+    {
+        return array_key_exists($index, $this->productsForComparsion) ? Product::find($this->productsForComparsion[$index]) : null;
+    }
+
+    /*public function nextFirstProduct($index)
+    {
+        $this->firstProductForComparsion = $this->getProduct($index);
+        $this->showModal(true);
+    }
+
+    public function prevFirstProduct($index)
+    {
+        $this->firstProductForComparsion = $this->getProduct($index);
+        $this->showModal(true);
+    }
+
+    public function nextSecondProduct($index)
+    {
+        $this->secondProductForComparsion = $this->getProduct($index);
+        $this->showModal(true);
+    }
+
+    public function prevSecondProduct($index)
+    {
+        $this->secondProductForComparsion = $this->getProduct($index);
+        $this->showModal(true);
+    }*/
+
+    public function changeFirstProductForComparsion($index)
+    {
+        $this->firstProductForComparsion = $this->getProduct($index);
+        $this->showModal(true);
+    }
+
+    public function changeSecondProductForComparsion($index)
+    {
+        $this->secondProductForComparsion = $this->getProduct($index);
+        $this->showModal(true);
+    }
+
+
+
+    protected function showModal($show)
+    {
+        $this->isModalShown = $show;
     }
 }
--- resources/views/livewire/index-page.blade.php
+++ resources/views/livewire/index-page.blade.php
@@ -31,7 +31,7 @@
         @endif
     </div>
 
-    <div @class(['modal','fade','show' => $productDeletedFromComparsion]) id="exampleModalCenter" tabindex="-1" aria-modal="true" role="dialog" @if($productDeletedFromComparsion) style="display: block" @endif>
+    <div @class(['modal','fade','show' => $isModalShown]) id="exampleModalCenter" tabindex="-1" aria-modal="true" role="dialog" @if($isModalShown) style="display: block" @endif>
         <div class="modal-dialog modal-dialog-centered" style="max-width: 800px">
             <div class="modal-content">
                 <div class="modal-header">
@@ -39,25 +39,51 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    @if(count($productsForComparsion) > 0)
-                    <div class="d-flex justify-content-between">
-                        @foreach($productsForComparsion as $i => $product)
-                        <div class="card mb-3 p-0 mx-1" style="flex: 1">
-                            <img src="{{ get_image_path($product['image'])  }}" class="card-img-top" alt="{{ $product['name'] }}">
-                            <div class="card-body d-flex flex-column">
-                                <h5 class="card-title">{{ $product['name'] }}</h5>
-                                <p class="card-text">{{ $product['description'] }}</p>
-                                <p class="card-text">Категория: <span class="text-primary">{{ $product['category']['name'] }}</span></p>
-                                <p class="card-text">Цена: <span class="text-primary">Цена {{ number_format($product['price'], 2, ',', ' ') }} р.</span></p>
-                            </div>
-                            <div class="card-buttons mt-auto">
-                                <button type="button" class="btn btn-danger d-block mt-2 w-100" wire:click="deleteFromComparison({{ $i }})" wire:key="{{ $i }}">Удалить</button>
-                            </div>
-                        </div>
-                        @endforeach
-                    </div>
-                    @else
+                    @if(is_null($firstProductForComparsion) && is_null($secondProductForComparsion))
                         <h4 class="text-primary text-center">Товаров для сравнения нет</h4>
+                    @else
+                        <div class="row">
+                        @if(!is_null($firstProductForComparsion))
+                            <div class="card mb-3 p-0 col-6">
+                                <img src="{{ get_image_path($firstProductForComparsion['image'])  }}" class="card-img-top" alt="{{ $firstProductForComparsion['name'] }}">
+                                <div class="card-body d-flex flex-column">
+                                    <h5 class="card-title">{{ $firstProductForComparsion['name'] }}</h5>
+                                    <p class="card-text">{{ $firstProductForComparsion['description'] }}</p>
+                                    <p class="card-text">Категория: <span class="text-primary">{{ $firstProductForComparsion['category']['name'] }}</span></p>
+                                    <p class="card-text">Цена: <span class="text-primary">Цена {{ number_format($firstProductForComparsion['price'], 2, ',', ' ') }} р.</span></p>
+
+                                </div>
+                                <div class="card-buttons mt-auto">
+                                    <h4 class="text-primary text-center">{{ $firstIndex + 1 }} из {{ count($productsForComparsion) }}</h4>
+                                    <div class="d-flex">
+                                        <button type="button" class="btn btn-primary" style="flex: 1; margin-right: 5px;" wire:click="changeFirstProductForComparsion({{ $firstIndex === 0 ? ($secondIndex === (count($productsForComparsion) - 1) ? (count($productsForComparsion) - 2) : (count($productsForComparsion) - 1)) : ($firstIndex - 1 === $secondIndex ? (($firstIndex - 2) >= 0 ? ($firstIndex - 2) : (count($productsForComparsion) - 1)) : ($firstIndex - 1)) }})">Предыдущий</button>
+                                        <button type="button" class="btn btn-primary" style="flex: 1; margin-left: 5px;" wire:click="changeFirstProductForComparsion({{ ($firstIndex + 1) === count($productsForComparsion) ? ($secondIndex === 0 ? 1 : 0) : (($firstIndex + 1) === $secondIndex ? (($firstIndex + 2) <= (count($productsForComparsion) - 1) ? ($firstIndex + 2) : 0) : ($firstIndex + 1)) }})">Следующий</button>
+                                    </div>
+                                    <button type="button" class="btn btn-danger d-block mt-2 w-100" wire:click="deleteFromComparison({{ $firstProductForComparsion['id'] }})">Удалить</button>
+                                </div>
+                            </div>
+                        @endif
+                        @if(!is_null($secondProductForComparsion))
+                            <div class="card mb-3 p-0 col-6">
+                                <img src="{{ get_image_path($secondProductForComparsion['image'])  }}" class="card-img-top" alt="{{ $secondProductForComparsion['name'] }}">
+                                <div class="card-body d-flex flex-column">
+                                    <h5 class="card-title">{{ $secondProductForComparsion['name'] }}</h5>
+                                    <p class="card-text">{{ $secondProductForComparsion['description'] }}</p>
+                                    <p class="card-text">Категория: <span class="text-primary">{{ $secondProductForComparsion['category']['name'] }}</span></p>
+                                    <p class="card-text">Цена: <span class="text-primary">Цена {{ number_format($secondProductForComparsion['price'], 2, ',', ' ') }} р.</span></p>
+
+                                </div>
+                                <div class="card-buttons mt-auto">
+                                    <h4 class="text-primary text-center">{{ $secondIndex + 1 }} из {{ count($productsForComparsion) }}</h4>
+                                    <div class="d-flex">
+                                        <button type="button" class="btn btn-primary" style="flex: 1; margin-right: 5px;" wire:click="changeSecondProductForComparsion({{ $secondIndex === 0 ? ($firstIndex === (count($productsForComparsion) - 1) ? (count($productsForComparsion) - 2) : (count($productsForComparsion) - 1)) : ($secondIndex - 1 === $firstIndex ? (($secondIndex - 2) >= 0 ? ($secondIndex - 2) : (count($productsForComparsion) - 1)) : ($secondIndex - 1)) }})">Предыдущий</button>
+                                        <button type="button" class="btn btn-primary" style="flex: 1; margin-left: 5px;" wire:click="changeSecondProductForComparsion({{ ($secondIndex + 1) === count($productsForComparsion) ? ($firstIndex === 0 ? 1 : 0) : (($secondIndex + 1) === $firstIndex ? (($secondIndex + 2) <= (count($productsForComparsion) - 1) ? ($secondIndex + 2) : 0) : ($secondIndex + 1)) }})">Следующий</button>
+                                    </div>
+                                    <button type="button" class="btn btn-danger d-block mt-2 w-100" wire:click="deleteFromComparison({{ $secondProductForComparsion['id'] }})">Удалить</button>
+                                </div>
+                            </div>
+                        @endif
+                        </div>
                     @endif
                 </div>
             </div>
